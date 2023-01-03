8b758c9c3a77344c37bd3d2fd065851fe53b03e7
Roberto <robertosevilla58@yahoo.es>
2023-01-03 23:10:12 +0100

    Add Pages

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+import PIL
+import pandas
+
+st.header("The Best Company!!!")
+visionTetra = """La vision de la compañia es vender bueno, bonito y barato. Pagar bien y chao pescao"""
+
+st.subheader(visionTetra)

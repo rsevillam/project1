10c0c7f1079c75ba0b819d8db33854b7f8925548
Roberto <robertosevilla58@yahoo.es>
2023-01-04 23:39:12 +0100

    Contact Us commit

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -1,5 +1,12 @@
 import streamlit as st 
 
-
+st.set_page_config(layout="wide")
 st.header("Contacto")
-st.subheader("Correo@correo.com")
+st.subheader("Correo electronico")
+with st.form(key="emailContacto"):
+    emailUsuario =st.text_input("Ingrese su correo electronico")
+    info = st.text_area("Escriba su comentario o sugerencia")
+    boton = st.form_submit_button("Enviar")
+
+if boton:
+    print("hola") 

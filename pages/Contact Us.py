c01558781dd8c960d8572ff6209a4957ef73c516
Roberto <robertosevilla58@yahoo.es>
2023-01-05 23:08:36 +0100

    Final Commit

--- pages/Contact Us.py
+++ pages/Contact Us.py
@@ -1,20 +1,23 @@
 import streamlit as st 
 from send_email import enviaCorreo
+import pandas 
 
 st.set_page_config(layout="wide")
 st.header("Contacto")
 st.subheader("Correo electronico")
 
+dataframe = pandas.read_csv('topics.csv')
+
 with st.form(key="emailContacto"):
     emailUsuario =st.text_input("Ingrese su correo electronico")
     topic = st.selectbox(
-        'Seleccione Tema por el que desea contactarnos', 
-        ('Ofertas de trabajo', 'Propuestas de Negocios', "Reclamaciones"))
+        'Seleccione Tema por el que desea contactarnos', options=dataframe)
     rawMensaje = st.text_area("Escriba su comentario o sugerencia")
     mensaje = f"""\
 Subject: Nuevo correo de {emailUsuario} 
 
 From: {emailUsuario}
+Topico Seleccionado: {topic}
 {rawMensaje}
 """
     boton = st.form_submit_button("Enviar")
@@ -22,3 +25,14 @@ From: {emailUsuario}
 if boton:
     enviaCorreo(mensaje)
     st.info("Su email fue enviado con éxito.")
+
+
+
+
+
+    """Solucion de Arpit
+    with st.form(key="emailContacto"):
+    emailUsuario =st.text_input("Ingrese su correo electronico")
+    topic = st.selectbox(
+        'Seleccione Tema por el que desea contactarnos', dataframe["topic"])
+    Mi solucion no seria correcta si el archivo .csv tuviera mas de 1 columna"""
